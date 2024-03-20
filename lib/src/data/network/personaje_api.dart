b43efd0e144a39559596257f4f12f21a1fda967b
sochoall <sochoa@libelulasoft.com>
2024-03-20 14:41:23 -0500

    Creacion de la clase abstracta API y clase Personaje API para obtener los personajes desde la API

---
+++ lib/src/data/network/personaje_api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:star_wars/src/data/dto/persona_dto.dart';
+
+abstract class Api {
+  Future<List<PersonajeDto>> getListaPersonajes();
+}
+
+class PersonajeApi implements Api {
+  @override
+  Future<List<PersonajeDto>> getListaPersonajes() async {
+    final response = await http
+        .get(Uri.parse('https://swapi.dev/api/people/?page=2&format=json'));
+    final Map personajes = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      return (personajes["results"] as List)
+          .map((e) => PersonajeDto.fromMap(e))
+          .toList();
+    } else {
+      throw Exception('Failed to fetch data. Please try again later.');
+    }
+  }
+}
