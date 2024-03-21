4c4eee8a37b71fd332875b217bc9aa2355a7d20d
sochoall <sochoa@libelulasoft.com>
2024-03-21 13:03:14 -0500

    REFACTOR: Escuchamos cuando el scroll llega al final de la pantalla para cargar mas datos

--- lib/src/presentation/provider/personajes_page/view/personaje_page.dart
+++ lib/src/presentation/provider/personajes_page/view/personaje_page.dart
@@ -49,27 +49,95 @@ class _Content extends StatefulWidget {
 
 class __ContentState extends State<_Content> {
   final _scrollController = ScrollController();
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
 
   @override
   Widget build(BuildContext context) {
     final lista =
         context.select((PersonajePageChangeNotifier b) => b.personaje);
+    final finalizo =
+        context.select((PersonajePageChangeNotifier b) => b.finalizo);
 
-    return Padding(
-      padding: const EdgeInsets.only(left: 16, right: 16),
-      child: ListView.builder(
-        key: const ValueKey('personaje_pagina_lista_key'),
-        controller: _scrollController,
-        itemCount: lista.length,
-        itemBuilder: (context, index) {
-          if (index >= lista.length) {
-            return const PersonajeItemLoading();
-          }
-          return Column(children: [
-            PersonajeInfoDetalles(personaje: lista[index]),
-          ]);
-        },
+    return SafeArea(
+      top: true,
+      bottom: true,
+      child: Column(
+        children: [
+          Card(
+            child: ListTile(
+              leading: const Icon(Icons.search),
+              title: TextField(
+                controller: _textController,
+                decoration: const InputDecoration(
+                  hintText: 'Filtrar por Género',
+                  border: InputBorder.none,
+                ),
+                onChanged: (value) {
+                  context
+                      .read<PersonajePageChangeNotifier>()
+                      .filtrarLista(value);
+                },
+              ),
+              trailing: IconButton(
+                icon: const Icon(Icons.cancel),
+                onPressed: () {
+                  _textController.clear();
+                  context.read<PersonajePageChangeNotifier>().filtrarLista('');
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: ListView.builder(
+                controller: _scrollController,
+                key: const ValueKey('personaje_pagina_lista_key'),
+                itemCount: finalizo ? lista.length : lista.length + 1,
+                itemBuilder: (context, index) {
+                  if (index >= lista.length) {
+                    return !finalizo
+                        ? const PersonajeItemLoading()
+                        : const SizedBox();
+                  }
+                  return Column(children: [
+                    PersonajeInfoDetalles(personaje: lista[index]),
+                  ]);
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController
+      ..removeListener(_onScroll)
+      ..dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_isBottom) {
+      context.read<PersonajePageChangeNotifier>().siguientePagina();
+    }
+  }
+
+  bool get _isBottom {
+    if (!_scrollController.hasClients) {
+      return false;
+    }
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    return currentScroll >= maxScroll;
   }
 }
