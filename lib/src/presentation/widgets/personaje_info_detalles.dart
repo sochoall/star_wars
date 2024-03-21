27556acc085c3b56951297e394988d18b2c40dbc
sochoall <sochoa@libelulasoft.com>
2024-03-21 13:04:06 -0500

    UPDATE: Agregamos nuevos campos para mostrar mas detalles de los personajes

--- lib/src/presentation/widgets/personaje_info_detalles.dart
+++ lib/src/presentation/widgets/personaje_info_detalles.dart
@@ -17,114 +17,142 @@ class PersonajeInfoDetalles extends StatelessWidget {
 
     return Column(
       children: [
-        Card(
-          elevation: 0,
-          color: cs.tertiaryContainer,
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 4),
-                child: Center(
-                  child: Text("Detalles del Peronaje"),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const CircleAvatar(
-                      radius: 40,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(child: Icon(Icons.photo)),
+        SizedBox(
+          height: 200,
+          child: Card(
+            elevation: 0,
+            color: cs.tertiaryContainer,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 4),
+                  child: Center(
+                    child: Text(
+                      "Detalles del Personaje",
+                      style: tt.titleMedium!.copyWith(
+                        color: cs.inversePrimary,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: [
-                                  Text(
-                                    'Nombre : ${personaje.name ?? ''}',
-                                    style: tt.titleSmall!.copyWith(
-                                      color: cs.onSurfaceVariant,
-                                      fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 6.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const CircleAvatar(
+                        radius: 40,
+                        child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: FittedBox(child: Icon(Icons.photo)),
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(6.0),
+                              child: Padding(
+                                padding: const EdgeInsets.all(2.0),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Text(
+                                      'Nombre : ${personaje.name ?? ''}',
+                                      style: tt.titleSmall!.copyWith(
+                                        color: cs.onSurfaceVariant,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(height: 4),
-                                  Text(
-                                    'Género: ${personaje.gender ?? ''}',
-                                    style: tt.titleMedium!.copyWith(
-                                      color: personaje.gender == 'male'
-                                          ? Colors.lightGreen
-                                          : Colors.redAccent,
+                                    const SizedBox(height: 4),
+                                    Text(
+                                      'Género: ${personaje.gender ?? ''}',
+                                      style: tt.titleMedium!.copyWith(
+                                        color: personaje.gender == 'masculino'
+                                            ? Colors.lightGreen
+                                            : personaje.gender == 'femenino'
+                                                ? Colors.redAccent
+                                                : Colors.amber,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(height: 4),
-                                  const Divider(
-                                    height: 1,
-                                    color: Colors.amber,
-                                    thickness: 0.5,
-                                  ),
-                                ],
+                                    const SizedBox(height: 4),
+                                    Text(
+                                      'Altura: ${personaje.height ?? ''}',
+                                      style: tt.titleSmall!.copyWith(
+                                        color: cs.onSurfaceVariant,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Text(
+                                      'Año de nacimiento: ${personaje.birthYear ?? ''}',
+                                      style: tt.titleSmall!.copyWith(
+                                        color: cs.onSurfaceVariant,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 4),
+                                    const Divider(
+                                      height: 1,
+                                      color: Colors.amber,
+                                      thickness: 0.5,
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Películas:',
+                    style: tt.bodyLarge!.copyWith(
+                      color: cs.onSurfaceVariant,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: personaje.films?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      final film = personaje.films![index];
+                      final nombre = film.split('/')[5];
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 2.0, bottom: 4),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(16),
+                            ),
+                            color: cs.surfaceVariant,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 16.0),
+                          height: 80,
+                          width: 80,
+                          child: Center(
                             child: Text(
-                              'Películas:',
+                              nombre,
                               style: tt.bodyLarge!.copyWith(
                                 color: cs.onSurfaceVariant,
                               ),
                             ),
                           ),
-                          SizedBox(
-                            height: 30,
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: personaje.films?.length ?? 0,
-                              itemBuilder: (context, index) {
-                                final film = personaje.films![index];
-                                final nombre = film.split('/')[5];
-                                return Padding(
-                                  padding:
-                                      const EdgeInsets.only(left: 12.0, top: 8),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: const BorderRadius.all(
-                                        Radius.circular(16),
-                                      ),
-                                      color: cs.surfaceVariant,
-                                    ),
-                                    height: 80,
-                                    width: 80,
-                                    child: Center(
-                                      child: Text(
-                                        nombre,
-                                        style: tt.bodyLarge!.copyWith(
-                                          color: cs.onSurfaceVariant,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
         ),
         Padding(
