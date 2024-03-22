11cdd3d4d4c31e2ce0732079af11145fa0edc0b7
sochoall <sochoa@libelulasoft.com>
2024-03-22 09:04:44 -0500

    REFACTOR: eliminada la sobrecapa de MAterialAPP

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Material App',
-      home: ListRoot(),
-    );
+    return const ListRoot();
   }
 }
